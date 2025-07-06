9498de5e47c8a6598f34bb077c86e3bbae2323e2
Sneha Kumawat <114502282+snehakumawat72@users.noreply.github.com>
2025-07-06 15:36:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (41.86%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -4,15 +4,9 @@ public:
      if(x<0) return false;  
 
      int org=x;
-     int rev=0;
+     long rev=0;
      while(x>0){
-        int ld=x%10;
-         // Check for overflow before multiplying by 10
-            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && ld > 7)) {
-                return false;  // If reversing would overflow, it can't be a palindrome
-            }
-            
-        rev= rev*10+ld;
+        rev= rev*10+x%10;;
         x=x/10;   
      }
     return rev==org;
