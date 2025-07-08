810e66b22880cfcc0b50f214a430521a127faccc
Sneha Kumawat <114502282+snehakumawat72@users.noreply.github.com>
2025-07-09 00:26:23 +0530

    Time: 79 ms (47.40%) | Memory: 164.7 MB (25.15%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    // Time: O(n^2), Space: O(1)
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+        
+        int start = 0, maxLen = 1;
+        
+        for (int i = 0; i < s.length(); i++) {
+            // Check for odd length palindromes (center at i)
+            int len1 = expandAroundCenter(s, i, i);
+            // Check for even length palindromes (center between i and i+1)
+            int len2 = expandAroundCenter(s, i, i + 1);
+            
+            int len = max(len1, len2);
+            if (len > maxLen) {
+                maxLen = len;
+                start = i - (len - 1) / 2;
+            }
+        }
+        
+        return s.substr(start, maxLen);
+    }
+    
+private:
+    int expandAroundCenter(string s, int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+};
